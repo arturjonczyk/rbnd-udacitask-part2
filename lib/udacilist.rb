8ffcda53746d7e1565578d2577d725e4daa5eafb
Artur Jonczyk <artur.jonczyk@gmail.com>
2016-05-13 21:53:00 +0200

    Refactored the delete method to allow deletion of multiple items

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -1,11 +1,12 @@
 class UdaciList
     attr_reader :title, :items
 
-    def initialize(options={})
-        @title = options[:title] || "Untitled List"
+    def initialize(options = {})
+        @title = options[:title] || 'Untitled List'
         @items = []
     end
-    def add(type, description, options={})
+
+    def add(type, description, options = {})
         type = type.downcase
         case type
         when 'todo'
@@ -15,43 +16,51 @@ class UdaciList
         when 'link'
             @items.push LinkItem.new(type, description, options)
         else
-            raise UdaciListErrors::InvalidItemType, "The type of provided task is invalid."
+            raise UdaciListErrors::InvalidItemType, 'The type of provided task is invalid.'
         end
     end
-    def delete(index)
-        if good_index?(index)
-            @items.delete_at(index - 1)
-        else
-            raise UdaciListErrors::IndexExceedsListSize, "Invalid index number exceeds the size of the list."
+
+    def delete(*args)
+        for index in args
+            if good_index?(index)
+                @items.delete_at(index - 1)
+                @items.insert(index - 1, nil)
+            else
+                raise UdaciListErrors::IndexExceedsListSize, 'Invalid index number exceeds the size of the list.'
+            end
         end
+        @items.delete_if(&:nil?)
     end
+
     def all
         rows = []
         @items.each_with_index do |item, position|
-            rows << ["#{position + 1}", item.details]
+            rows << [(position + 1).to_s, item.details]
         end
         table = Terminal::Table.new(title: @title.colorize(:magenta), rows: rows)
         table.align_column(0, :center)
         puts table
     end
+
     def filter(type)
         results = @items.select { |task| task.type == type }
-        results.size > 0 ? results : "There aren't any items of '#{type}' type."
+        !results.empty? ? results : "There aren't any items of '#{type}' type."
     end
 
     def change_priority(index, priority)
         if good_index?(index)
             if @items[index - 1].type == 'todo'
-                @items[index-1].change(priority)
+                @items[index - 1].change(priority)
             else
-                raise UdaciListErrors::InvalidItemType, "The type of provided task is invalid."
+                raise UdaciListErrors::InvalidItemType, 'The type of provided task is invalid.'
             end
         else
-            raise UdaciListErrors::IndexExceedsListSize, "Invalid index number exceeds the size of the list."
+            raise UdaciListErrors::IndexExceedsListSize, 'Invalid index number exceeds the size of the list.'
         end
     end
 
     private
+
     def good_index?(index)
         @items.size >= index
     end
